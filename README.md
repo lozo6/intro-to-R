e1d27375bf2cdf750ae0d85500c5e9d47199d3be
lozo6 <99729303+lozo6@users.noreply.github.com>
2022-10-28 10:56:07 -0400

    renamed files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # introR
 HHA 507 Assignment 8
+
+Part 1: hbctraining.R
+
+Part 2: introstats.R
---
+++ hbctraining.R
@@ -0,0 +1,20 @@
+# Part 1.1
+
+x <- 3
+
+y <- 5
+
+y
+
+x + y
+
+number <- x + y
+
+x <- 5
+
+y <- 10
+
+# Try changing the value of the variable x to 5. What happens to number?
+## number would now be 5 + 5 = 10
+#Now try changing the value of variable y to contain the value 10. What do you need to do, to update the variable number?
+## You need to assign y <- 10
